72ed63be03526e60ff1e41d1c84193135c7db649
hebaarafat159 <heba.arafat159@gmail.com>
2023-12-10 22:58:09 +0000

    skills

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Typography, Card, Stack, Box } from '@mui/joy';
 
 export default function Skill({ skill }) {
     return (
-        <>
-            <h1> Skill Page </h1>
-            <div className="skill">    
-                <FontAwesomeIcon icon={skill.icon} />
-                <span>{skill.name}</span>
-            </div>
-        </>
+        <Card sx={{ maxWidth: '20vh'}}>
+            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 1.5 }}>
+                <Stack direction="row" spacing={1}>
+                    <Stack direction="column" spacing={1}>
+                        <FontAwesomeIcon icon={skill.icon} />
+                        <Stack spacing={1} sx={{ flexGrow: 1 }}>
+                            <Typography component="h1" variant="h1" style={{ fontSize: '1.5vw' }}> {skill.name} </Typography>
+                        </Stack>
+                    </Stack>
+                </Stack>
+            </Box>
+        </Card>
     );
 }
--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -1,14 +1,23 @@
 // import { faCode } from '@fortawesome/free-solid-svg-icons'; // You can import more icons as needed
 import Skill from './Skill'
-import { faReact, faNode, faHtml5, faCss3, faJs } from '@fortawesome/free-brands-svg-icons';
+import { faReact, faNode, faHtml5, faCss3, faSquareJs, faAndroid, faGitSquare, faNodeJs, faMdb, faPython } from '@fortawesome/free-brands-svg-icons';
 
 export default function SkillList() {
     const skills = [
         { name: 'React', icon: faReact },
         { name: 'Node.js', icon: faNode },
         { name: 'HTML5', icon: faHtml5 },
-        { name: 'CSS3', icon: faCss3 },
-        { name: 'JavaScript', icon: faJs }
+        { name: 'CSS', icon: faCss3 },
+        { name: 'JavaScript', icon: faSquareJs },
+        { name: 'NextJs', icon: faNodeJs },
+        { name: 'Android', icon: faAndroid },
+        { name: 'Express', icon: faNodeJs },
+        { name: 'Python3', icon: faPython },
+        { name: 'Django', icon: faPython },
+        { name: 'MongoDB', icon: faMdb },
+        { name: 'PostgreSQL', icon: faMdb },
+        { name: 'SQLite', icon: faMdb },
+        { name: 'Git', icon: faGitSquare },
     ]
 
     return (
