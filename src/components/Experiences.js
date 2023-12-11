339641496d91ac8b773773f7dc26d2018ec09fcf
hebaarafat159 <heba.arafat159@gmail.com>
2023-12-11 23:16:50 +0000

    experience

---
+++ src/components/Experiences.js
@@ -0,0 +1,113 @@
+import { List, Divider, Typography, Box, Chip, ListItemDecorator, ListItemContent, ListItem } from "@mui/joy";
+export default function Experiences() {
+    const experiences = [
+        {
+            id: 1,
+            compnayName: '\tGeneral Assembly',
+            location: 'London, UK',
+            role: 'Student, Software Engineering Immersive',
+            duration: 'September 2023 - November 2023',
+            description: 'A 3-month full-time bootcamp taken remotely using Zoom and Slack. It consisted of daily standups, lectures, workshops, homework and final projects. Covered the fundamentals of web development from frontend to backend.',
+            skills: []
+        },
+        {
+            id: 2,
+            compnayName: 'Worc Company',
+            location: 'Cairo, Egypt ',
+            role: 'Freelance Android Software Developer',
+            duration: 'Apr 2016 - May 2016 ',
+            description: '',
+            skills: []
+        },
+        {
+            id: 3,
+            compnayName: 'Tech-World Companyy',
+            location: 'Cairo, Egypt',
+            role: 'Senior Software Developer',
+            duration: 'Jan 2015 - Apr 2016',
+            description: '',
+            skills: []
+        },
+        {
+            id: 4,
+            compnayName: 'National Technology Company',
+            location: 'Cairo, Egypt',
+            role: 'Freelance Android Software Developer',
+            duration: 'Oct 2014 - Jan 2015',
+            description: '',
+            skills: []
+        },
+        {
+            id: 5,
+            compnayName: 'ITWorx Company',
+            location: 'Cairo, Egypt',
+            role: 'Android Senior Software Developer',
+            duration: 'Dec 2012 - Mar 2014',
+            description: '',
+            skills: []
+        },
+        {
+            id: 6,
+            compnayName: 'CIT Mobidiv Company',
+            location: 'Cairo, Egypt',
+            role: 'Android Software Developer',
+            duration: 'Jul 2010 - Dec 2012',
+            description: '',
+            skills: []
+        },
+    ]
+    return (
+        <>
+            <List sx={{ '--ListItemDecorator-size': '40px', gap: 2 }}>
+                {experiences.map((experience, index) => (
+                    <>
+                        <ListItem key={index} sx={{ alignItems: 'flex-start' }}>
+                            <ListItemDecorator
+                                sx={{
+                                    '&:before': {
+                                        content: '""',
+                                        position: 'absolute',
+                                        height: '100%',
+                                        width: '2px',
+                                        bgcolor: 'divider',
+                                        left: 'calc(var(--ListItem-paddingLeft) + 20px)',
+                                        top: '50%',
+                                    },
+                                }}>
+                                {/* <Avatar
+                                    src={company.logo}
+                                    sx={{ '--Avatar-size': '24px' }}
+                                /> */}
+                            </ListItemDecorator>
+                            <ListItemContent>
+                                <Typography level="title-sm">{experience.role}</Typography>
+                                <Typography level="body-xs">{experience.compnayName}</Typography>
+                                <Typography level="body-xs">{experience.description}</Typography>
+                            </ListItemContent>
+                            <Typography level="body-xs">{experience.duration}</Typography>
+                            
+                        </ListItem>
+                        {experience.skills && experience.skills.length > 0 ?
+                            <>
+                                <Divider component="div" sx={{ my: 2 }} />
+                                <Typography level="title-sm">Skills tags:</Typography>
+                                <Box sx={{ mt: 1.5, display: 'flex', gap: 1 }}>
+                                    {experience.skills.map((skill, skillIndex) => (
+                                        <Chip
+                                            key={skillIndex}
+                                            variant="outlined"
+                                            color="neutral"
+                                            size="sm"
+                                        >
+                                            {skill}
+                                        </Chip>
+                                    ))}
+                                </Box></> : null}
+
+                    </>
+                ))}
+            </List>
+
+        </>
+    );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import Typography from '@mui/joy/Typography'
 import { Stack, Card } from '@mui/joy'
 import { TypeAnimation } from 'react-type-animation';
-
+import Experiences from '../components/Experiences'
 export default function Home() {
     return (
         <Card >
@@ -33,29 +33,15 @@ export default function Home() {
                     </Stack>
                 </Stack>
             </Stack>
+            <Stack
+                direction="column"
+                spacing={2}
+                sx={{ display: { xs: 'flex' }, my: 1 }}>
+                <Stack spacing={1} sx={{ flexGrow: 1 }}>
+                    <Typography component="h1" variant="h1" style={{ fontSize: '1.5vw' }}> Experiences </Typography>
+                    <Experiences />
+                </Stack>
+            </Stack>
         </Card>
-        // <Box style={{ display: 'flex', flexFlow: 'row wrap', gap: '4vmin', margin: '20vmin' }} size="lg" sx={{ width: 'auto', height: 'auto', '&:hover': { boxShadow: 'md' } }}>
-        //     <Stack direction="row" spacing={2}>
-        //         <img src={'profile_img.jpeg'} className='profileImage' loading="lazy" alt="" width={200} height={200} />
-        //         <Stack spacing={1} sx={{ display: 'flex', flexFlow: 'column wrap', gap: '4vmin', padding: '5vmin' }}>
-        //             <Typography component="h1" variant="h1" style={{ fontSize: '1.5vw' }}> Heba Arafat </Typography>
-        //             <Typography component="h1" variant="h1" style={{ fontSize: '1.5vw' }}>
-        //                 I'm a  <TypeAnimation
-        //                     sequence={[
-        //                         'Software Developer',
-        //                         1000,
-        //                         'Android Developer',
-        //                         1000,
-        //                         'Full Stack Developer',
-        //                         1000,
-        //                     ]}
-        //                     speed={30}
-        //                     style={{ fontSize: '2vw' }}
-        //                     repeat={Infinity}
-        //                 />
-        //             </Typography>
-        //         </Stack>
-        //     </Stack>
-        // </Box>
     );
 }
