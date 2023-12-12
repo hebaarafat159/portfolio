a7e6abbe24558cf496894081c599cdf3ca87165f
hebaarafat159 <heba.arafat159@gmail.com>
2023-12-12 01:31:47 +0000

    add gmail link

--- src/components/ContactsMe.js
+++ src/components/ContactsMe.js
@@ -4,7 +4,7 @@ export default function Contacts() {
         <>
             <SocialIcon network="linkedin" url = 'https://www.linkedin.com/in/hebaarafat'/>
             <SocialIcon network="github" url = 'https://github.com/hebaarafat159'/>
-            <SocialIcon network="email" url='heba.arafat159@gmail.com'/>
+            <SocialIcon network="email" url='mailto:heba.arafat159@gmail.com' />
         </>
     );
 }
